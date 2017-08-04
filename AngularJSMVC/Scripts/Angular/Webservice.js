b48a0c06f8390ad10b8d206283cdd2e69b8aef9a
xuqianshawn <xuqianshawn@gmail.com>
2017-08-04 17:18:50 +0800

    refactor

---
+++ AngularJSMVC/Scripts/Angular/Webservice.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    $.ajax({
+        type: "POST",
+        url: "/WebService/COEService.asmx/HelloWorld",
+        data: JSON.stringify({ "Name": "name", "MarketShare": 12.5 }),
+        success: function (data) {
+            var jsondata = JSON.parse(data);
+            alert(jsondata);
+        },
+        error: function (err) {
+            alert("error");
+        },
+
+        dataType: 'json'
+    });
+});
