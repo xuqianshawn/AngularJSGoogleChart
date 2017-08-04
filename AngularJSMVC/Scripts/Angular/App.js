352d4b8f017c5251f67eb6d6488f9f5134740ae1
xuqianshawn <xuqianshawn@gmail.com>
2017-08-04 15:09:13 +0800

    Make it able to post data to web service

--- AngularJSMVC/Scripts/Angular/App.js
+++ AngularJSMVC/Scripts/Angular/App.js
@@ -1,9 +1,21 @@
-﻿(function () {
-    angular.module('demo', [])
-    .controller('HomeCtrl', function ($scope, $http) {
-        $http.get('/home/GetData').
-            then(function (response) {
-                $scope.stock = response.data;
-            });
+﻿$(function () {
+
+    $.ajax({
+        type: "POST",
+        url: "/WebService/COEService.asmx/HelloWorld",
+        data: { 'Name': '1', 'MarketShare': '1' },
+        success: function (data) {
+            console.log(data);
+        },
+        dataType: 'json'
     });
-})();
+
+
+    //angular.module('demo', [])
+    //.controller('HomeCtrl', function ($scope, $http) {
+    //    $http.get('/home/GetData').
+    //        then(function (response) {
+    //            $scope.stock = response.data;
+    //        });
+    //});
+});
---
+++ AngularJSMVC/WebService/COEService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="COEService.asmx.cs" Class="AngularJSMVC.WebService.COEService" %>
